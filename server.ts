86b7c7d5dca73cd2a96439ebc2b4ed0ad6cd462d
Noëllie Chatain Six <noellie.chatain@hotmail.com>
2024-06-19 09:52:55 +0200

    import main router

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 // Useful resource: how to set up TS with Node and Express: https://dev.to/cristain/how-to-set-up-typescript-with-nodejs-and-express-2023-gf
-import express, { Request, Response, Application } from 'express';
+import express, { Application } from 'express';
 
 require('dotenv').config();
 const cors = require('cors');
 const app: Application = express();
+import router from './src/routers/router';
 
 app.use(
   cors({
@@ -12,9 +13,14 @@ app.use(
   })
 );
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to my Express + TS Server');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', router);
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Welcome to my Express + TS Server');
+// });
 
 app.listen(process.env.PORT, () => {
   console.log(`App listening on: http://localhost:${process.env.PORT}`);
